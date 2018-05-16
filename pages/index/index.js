5398eb61e1cdad49f158db4ac0656800f336a17c
Storax <371872491@qq.com>
2018-05-16 11:52:03 +0800

    learn 4-4:动态数据绑定

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,25 +2,9 @@
 Page({
   data: {
     firstImage: "",
-    objectArray: [
-      { id: 5, unique: 'unique_5' },
-      { id: 4, unique: 'unique_4' },
-      { id: 3, unique: 'unique_3' },
-      { id: 2, unique: 'unique_2' },
-      { id: 1, unique: 'unique_1' },
-      { id: 0, unique: 'unique_0' },
-    ],
+    gn:'国内', gj:'国际', cj:'财经', yr:'娱乐', js:'军事', ty:'体育', qt:'其他',
   },
-  
-  // newstypeArray: [
-  //   { id: 'gn', unique: "国内" },
-  //   { id: 'gj', unique: '国际' },
-  //   { id: 'cj', unique: '财经' },
-  //   { id: 'yr', unique: '娱乐' },
-  //   { id: 'js', unique: '军事' },
-  //   { id: 'ty', unique: '体育' },
-  //   { id: 'qt', unique: '其他' },
-  // ],  
+
   onLoad() {
     wx.request({
       url: 'https://test-miniprogram.com/api/news/list',
